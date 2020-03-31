b858375648e13b4c807dbbdb75b097d1f1cd9fe2
DHaoYu <178070667@qq.com>
2020-03-31 23:18:24 +0800

    if_test

---
+++ shell/if_test/var1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#by authors dhy
+
+#var='my first bash'
+
+#echo '$var'
+#echo "this is $var"
+string="my name is dhy"
+
+#echo ${#string}
+#echo "${string:1:4}"
+
+arr=(1 2 3 val1 val2 val3)
+
+#echo "The first val is ${arr[0]}"
+echo "the length in arr is ${#arr[@]}"
